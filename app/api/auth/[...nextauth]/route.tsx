c19350fff5a5ddf4cb5fc36c4ec7df94a1f76984
Kaduk7 <zulakmal24@gmail.com>
2023-12-25 04:29:18 +0700

    fsdfsdf

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -2,11 +2,11 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import * as bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
-import { NextAuthOptions ,AuthOptions } from "next-auth";
+import { NextAuthOptions} from "next-auth";
 
 const prisma = new PrismaClient();
 
-export const authOptions: AuthOptions = {
+const authOptions: NextAuthOptions = {
 
   providers: [
     CredentialsProvider({
