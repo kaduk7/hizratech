152f5a18c088c8a93af13ea2e85e267f61b9fba8
Kaduk7 <zulakmal24@gmail.com>
2023-12-25 04:32:32 +0700

    dsdsd

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from './api/auth/[...nextauth]/route';
+// import { authOptions } from './api/auth/[...nextauth]/route';
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  // const session = await getServerSession(authOptions)
   return (
     <main>
       <h2 style={{ color: 'black' }}>Dashboard</h2>
-      <pre>{JSON.stringify(session)}</pre>
+      {/* <pre>{JSON.stringify(session)}</pre> */}
     </main>
 
   )
