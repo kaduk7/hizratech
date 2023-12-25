cc100e22c5fbb676084a9c451aac1c3f9ea4df47
Kaduk7 <zulakmal24@gmail.com>
2023-12-25 21:51:55 +0700

    dsd

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    eslint: {
-        dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-    },
-}
+module.exports = nextConfig
