fcf2cb2d7ce7e292c766e077645cc17cfb9be111
Kaduk7 <zulakmal24@gmail.com>
2023-12-26 13:28:44 +0700

    sssssss

--- middleware.tsx
+++ middleware.tsx
@@ -22,6 +22,7 @@ export async function middleware(request: NextRequest) {
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
   });
+  console.log('tes',token)
 
   if (token && pathname.match(loginPath)) {
     const url = new URL('/', request.url);
